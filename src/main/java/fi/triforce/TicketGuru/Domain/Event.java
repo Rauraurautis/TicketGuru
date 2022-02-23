76745882b180117a14db844e36355ca6b5d9b712
unknown <rautanokka@gmail.com>
2022-02-24 01:44:29 +0200

    lisätty relaatiot luokkien välille ja getterit, setterit ja constructorit vaihdettu lombok-kirjaston annotaatioihin

--- src/main/java/fi/triforce/TicketGuru/Domain/Event.java
+++ src/main/java/fi/triforce/TicketGuru/Domain/Event.java
@@ -4,12 +4,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.*;
+import java.util.List;
 
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Event {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
@@ -18,54 +30,11 @@ public class Event {
 	private long venueId;
 	private long numberOfTickets;
 	private LocalDate date;
-	
-	public Event(String eventDescription, long venueId, long numberOfTickets, LocalDate date) {
-		super();
-		this.eventDescription = eventDescription;
-		this.venueId = venueId;
-		this.numberOfTickets = numberOfTickets;
-		this.date = date;
-	}
-
-	public long getEventID() {
-		return eventID;
-	}
-
-	public void setEventID(long eventID) {
-		this.eventID = eventID;
-	}
-
-	public String getEventDescription() {
-		return eventDescription;
-	}
-
-	public void setEventDescription(String eventDescription) {
-		this.eventDescription = eventDescription;
-	}
-
-	public long getVenueId() {
-		return venueId;
-	}
-
-	public void setVenueId(long venueId) {
-		this.venueId = venueId;
-	}
-
-	public long getNumberOfTickets() {
-		return numberOfTickets;
-	}
-
-	public void setNumberOfTickets(long numberOfTickets) {
-		this.numberOfTickets = numberOfTickets;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+	@ManyToOne
+	@JoinTable(name = "venue_id")
+	private Venue eventVenue;
+	@OneToMany(mappedBy="event")
+	private List<TicketType> ticketTypes;
 
 	@Override
 	public String toString() {

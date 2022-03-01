b37953ffee866d842daff0dbffedef75279d8a68
IceCatDoge <miska.routio@hotmail.com>
2022-03-01 15:51:43 +0200

    lisätty data.sql resursseihin. koskettu app.prop, jotta create-drop eikä update, jotta toimii ns. aina

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO venue (venue_name, venue_address, venue_city) VALUES ('Sibeliustalo', 'Ankkurikatu 7, 15140', 'Lahti');
+INSERT INTO venue (venue_name, venue_address, venue_city) VALUES ('Tampere-talo', 'Yliopistonkatu 55, 33100', 'Tampere');
+INSERT INTO venue (venue_name, venue_address, venue_city) VALUES ('Nokia Areena', 'Sorinkatu 3, 33100', 'Tampere');
+INSERT INTO venue (venue_name, venue_address, venue_city) VALUES ('Tavastia', 'Urho Kekkosen katu 4, 00100', 'Helsinki');
+INSERT INTO event (event_description, number_of_tickets) VALUES ('Lady Gaga, Monster Tour', 3400);
+INSERT INTO event (event_description, number_of_tickets) VALUES ('Fröbelin Palikat, Never Stop The Madness', 250);
+INSERT INTO event (event_description, number_of_tickets) VALUES ('Alice Cooper, Poison Concert One Night Only', 2800);
+INSERT INTO event (event_description, number_of_tickets) VALUES ('Elvis, I Never Left Tour', 6000);

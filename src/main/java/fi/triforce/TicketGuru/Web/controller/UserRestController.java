96ad94e58f443ef0dc119ed8a3ec670da2ea4f0b
unknown <rautanokka@gmail.com>
2022-03-28 23:58:50 +0300

    lisätty käyttäjän delete

--- src/main/java/fi/triforce/TicketGuru/Web/controller/UserRestController.java
+++ src/main/java/fi/triforce/TicketGuru/Web/controller/UserRestController.java
@@ -4,16 +4,20 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import fi.triforce.TicketGuru.Domain.Role;
 import fi.triforce.TicketGuru.Domain.User;
 import fi.triforce.TicketGuru.Web.service.UserService;
+import fi.triforce.TicketGuru.exception.ResourceNotFoundException;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -34,6 +38,11 @@ public class UserRestController {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
         System.out.println(uri);
         return ResponseEntity.created(uri).body(us.saveUser(user));
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteuser(@PathVariable(name = "id") Long id) throws ResourceNotFoundException {
+        return ResponseEntity.ok(us.deleteUser(id));
     }
 
     @PostMapping("/role/addtouser")

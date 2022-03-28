a435a36680ba8fff3e7002bacebd748415cae1de
unknown <rautanokka@gmail.com>
2022-03-28 18:47:28 +0300

    lisätty security readme

--- src/main/java/fi/triforce/TicketGuru/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/fi/triforce/TicketGuru/security/filter/CustomAuthenticationFilter.java
@@ -66,14 +66,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
                 String access_token = JWT.create()
                                 .withSubject(user.getUsername())
-                                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                                .withIssuer(request.getRequestURL().toString())
-                                .withClaim("roles",
-                                                user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-                                                                .collect(Collectors.toList()))
-                                .sign(algorithm);
-                String refresh_token = JWT.create()
-                                .withSubject(user.getUsername())
                                 .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
                                 .withIssuer(request.getRequestURL().toString())
                                 .withClaim("roles",
@@ -81,14 +73,8 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                                                                 .collect(Collectors.toList()))
                                 .sign(algorithm);
 
-                /*
-                 * response.setHeader("access_token", access_token);
-                 * response.setHeader("refresh_token", refresh_token);
-                 */
-
                 Map<String, String> tokens = new HashMap<>();
                 tokens.put("access_token", access_token);
-                tokens.put("refresh_token", refresh_token);
                 response.setContentType(MediaType.APPLICATION_JSON_VALUE);
                 new ObjectMapper().writeValue(response.getOutputStream(), tokens);
         }

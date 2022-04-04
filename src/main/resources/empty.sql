8d093903ed53edf828e8d1b417d6404da7c930f3
cyph00n <jp.silkosuo@gmail.com>
2022-04-04 21:28:00 +0300

    a

--- src/main/resources/empty.sql
+++ src/main/resources/empty.sql
@@ -1,1 +1,2 @@
-SELECT * FROM event WHERE venue_name = 'asdf';
+INSERT INTO venue (venue_name, venue_address, venue_city) VALUES ('Sibeliustalo', 'Ankkurikatu 7, 15140', 'Lahti');
+DELETE FROM venue WHERE venue_name = 'Sibeliustalo';
